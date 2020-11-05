c3c0e05fc1585a21006bfaaaed0a4579899461fa
Jonas Eberle <flightvision@googlemail.com>
2020-11-05 19:56:20 +0100

    enable ddev development environment

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Structured Content Registration API',
+    'description' => '//TODO',
+    'category' => 'fe',
+    'author' => '',
+    'author_email' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-10.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
