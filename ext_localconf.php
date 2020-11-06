90e40b1b56a442ecf8f553a6c343fbfc5ba93c66
Jonas Eberle <flightvision@googlemail.com>
2020-11-06 14:21:28 +0100

    [TASK] Add cache to "system" group

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,9 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 (static function ($_EXTKEY = 'sci_api') {
     if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\Sci\SciApi\Constants::CACHE])) {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\Sci\SciApi\Constants::CACHE] = [];
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\Sci\SciApi\Constants::CACHE] = [
+            'groups' => ['system'],
+        ];
     }
 
     if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][\Sci\SciApi\Constants::CACHE]['frontend'])) {
