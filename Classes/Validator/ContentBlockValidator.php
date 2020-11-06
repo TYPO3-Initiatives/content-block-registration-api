4ccb9929b6a505a96ac1d3c53eed7103c6755663
Jonas Eberle <flightvision@googlemail.com>
2020-11-06 16:23:24 +0100

    [FEATURE] Add CB validation stub

---
+++ Classes/Validator/ContentBlockValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package sci/sci-api.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Sci\SciApi\Validator;
+
+class ContentBlockValidator
+{
+    /**
+     * Throws on validation error
+     * @param array $cbConfiguration
+     */
+    public function validate(array $cbConfiguration): void
+    {
+        // TODO
+    }
+}
