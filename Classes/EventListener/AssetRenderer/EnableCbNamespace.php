48390e338b79ac23a5338a3fe80fbd8cbbcf6f30
Jonas Eberle <flightvision@googlemail.com>
2020-11-09 00:03:21 +0100

    [TASK] Add header block

--- Classes/EventListener/AssetRenderer/EnableCbNamespace.php
+++ Classes/EventListener/AssetRenderer/EnableCbNamespace.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the package sci/sci-api.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace Sci\SciApi\EventListener\AssetRenderer;
 
 use Sci\SciApi\Constants;
