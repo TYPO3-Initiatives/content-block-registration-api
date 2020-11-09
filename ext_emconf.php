dbcc5598cdc950b463a088199449b3c366894ed0
Jonas Eberle <flightvision@googlemail.com>
2020-11-09 01:32:11 +0100

    [TASK] allow installation in TYPO3v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '10.4.0-11.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
