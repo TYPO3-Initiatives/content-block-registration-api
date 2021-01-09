f6fdd2606e35c59d4ccfdd2c1c2e5b678f4aa82a
Jonas Eberle <flightvision@googlemail.com>
2021-01-09 21:30:25 +0100

    [TASK] hide layout palette (fixes #24)

--- Classes/Generator/TcaGenerator.php
+++ Classes/Generator/TcaGenerator.php
@@ -115,7 +115,6 @@ class TcaGenerator
                             header,
                             content_block,
                         --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-                            --palette--;;frames,
                             --palette--;;appearanceLinks,
                         --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
                             --palette--;;language,
