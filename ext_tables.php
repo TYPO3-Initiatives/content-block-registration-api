d679b3c8f89ce641bfa1e3cf5a0c48103e97cf0c
Andre Kraus <info@andrekraus.com>
2022-04-04 15:49:02 +0200

    [FEATURE] disable Backend Module by extension settings

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,8 @@
 <?php
 
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3_MODE') || die('Access denied.');
@@ -10,25 +12,26 @@ call_user_func(
     function()
     {
 
-
         if (TYPO3_MODE === 'BE') {
 
-            ExtensionUtility::registerModule(
-                'Typo3Contentblocks.ContentblocksRegApi',
-                'tools', // Make module a submodule of 'web'
-                'wizard', // Submodule key
-                '', // Position
-                [
-                    \Typo3Contentblocks\ContentblocksRegApi\Backend\Controller\WizardController::class =>  'new, create',
-                ],
-                [
-                    'access' => 'admin',
-                    'icon'   => 'EXT:contentblocks_reg_api/Resources/Public/Icons/Extension.svg',
-                    'labels' => 'LLL:EXT:contentblocks_reg_api/Resources/Private/Language/locallang_db.xlf',
-                    'navigationComponentId' => '',
-                    'inheritNavigationComponentFromMainModule' => false
-                ]
-            );
+            if (!(bool) GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('contentblocks_reg_api')['disableBackendModul']) {
+                ExtensionUtility::registerModule(
+                    'Typo3Contentblocks.ContentblocksRegApi',
+                    'tools', // Make module a submodule of 'web'
+                    'wizard', // Submodule key
+                    '', // Position
+                    [
+                        \Typo3Contentblocks\ContentblocksRegApi\Backend\Controller\WizardController::class =>  'new, create',
+                    ],
+                    [
+                        'access' => 'admin',
+                        'icon'   => 'EXT:contentblocks_reg_api/Resources/Public/Icons/Extension.svg',
+                        'labels' => 'LLL:EXT:contentblocks_reg_api/Resources/Private/Language/locallang_db.xlf',
+                        'navigationComponentId' => '',
+                        'inheritNavigationComponentFromMainModule' => false
+                    ]
+                );
+            }
 
             /***************
              * Allow Custom Records on Standard Pages
