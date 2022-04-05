dec75d689f4761ba36f60c5d81f330b9eeed4047
Lidia Demin <typecat@gmx.net>
2022-04-05 15:07:39 +0200

    [BUGFIX] Remove deprecated condition

--- ext_tables.php
+++ ext_tables.php
@@ -6,32 +6,29 @@ defined('TYPO3') || die('Access denied.');
 call_user_func(
     static function()
     {
-        if (TYPO3_MODE === 'BE') {
-
-            if (!(bool) \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('contentblocks_reg_api')['disableBackendModul']) {
-                \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-                    'ContentblocksRegApi',
-                    'tools', // Make module a submodule of 'web'
-                    'wizard', // Submodule key
-                    '', // Position
-                    [
-                        \Typo3Contentblocks\ContentblocksRegApi\Backend\Controller\WizardController::class =>  'new, create',
-                    ],
-                    [
-                        'access' => 'admin',
-                        'icon'   => 'EXT:contentblocks_reg_api/Resources/Public/Icons/Extension.svg',
-                        'labels' => 'LLL:EXT:contentblocks_reg_api/Resources/Private/Language/locallang_db.xlf',
-                        'navigationComponentId' => '',
-                        'inheritNavigationComponentFromMainModule' => false
-                    ]
-                );
-            }
-
-            /**
-             * Allow Custom Records on Standard Pages
-             */
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_contentblocks_reg_api_collection');
-
+        if (!(bool) \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('contentblocks_reg_api')['disableBackendModul']) {
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'ContentblocksRegApi',
+                'tools', // Make module a submodule of 'web'
+                'wizard', // Submodule key
+                '', // Position
+                [
+                    \Typo3Contentblocks\ContentblocksRegApi\Backend\Controller\WizardController::class =>  'new, create',
+                ],
+                [
+                    'access' => 'admin',
+                    'icon'   => 'EXT:contentblocks_reg_api/Resources/Public/Icons/Extension.svg',
+                    'labels' => 'LLL:EXT:contentblocks_reg_api/Resources/Private/Language/locallang_db.xlf',
+                    'navigationComponentId' => '',
+                    'inheritNavigationComponentFromMainModule' => false
+                ]
+            );
         }
+
+        /**
+         * Allow Custom Records on Standard Pages
+         */
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_contentblocks_reg_api_collection');
+
     }
 );
